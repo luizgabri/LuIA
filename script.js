a71c9a2bdc4d94dc38db26a2c779ff2d3648dfe6
luizgabri <160794599+luizgabri@users.noreply.github.com>
2025-09-10 20:59:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-Perguntas");
+const caixaAlternativa=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+    const perguntas = [
+         {
+            enunciado:"Assim que voce saiu do colegio se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter tambem gera imagem e audio.Qual é o seu primeiro pensamento?",
+            alternativas:[
+                {
+                texto:"Isso e assustador!",
+                afirmacao:"No inicio,eu ficaria com medo do que essa tecnologia pode fazer."
+                }
+            ]
+        }
+    ]
